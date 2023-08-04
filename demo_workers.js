8868f903cb83c721f5ccc8361b208e56ed82be63
tekno-app <141418595+tekno-app@users.noreply.github.com>
2023-08-04 21:20:08 +0700

    Add files via upload

---
+++ demo_workers.js
@@ -0,0 +1,12 @@
+//postMessage('');
+postMessage(`<div class="my-1">
+            <div class="card">
+            <div class="card-body pb-0">
+              <blackquote class="text-secondary">
+                <p class="text-center">~ Belum banyak yang bisa dicari</p>
+              </blackquote>
+              <p class="text-center"><button class="btn btn-success" id="semua" onclick="searchInput.value = 'tampil semua',
+  searchBtn.click()">tampil semua</button></p>
+            </div>
+          </div>
+          </div>`);
